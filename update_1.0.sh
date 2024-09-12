cb4da2edeceb38dba3392221edf09520d28e98c0
srinivas-siripurapu <srinivas-siripurapu@users.noreply.github.com>
2024-09-12 20:41:33 +0530

    Update update_1.0.sh

--- update_1.0.sh
+++ update_1.0.sh
@@ -84,6 +84,12 @@ fi
 
 # Install Verilator (specific version)
 if ! command_exists "verilator"; then
+    # Prerequisites:
+    sudo apt-get install git help2man perl python3 make autoconf g++ flex bison ccache
+    sudo apt-get install libgoogle-perftools-dev numactl perl-doc
+    sudo apt-get install libfl2  # Ubuntu only (ignore if gives error)
+    sudo apt-get install libfl-dev  # Ubuntu only (ignore if gives error)
+    sudo apt-get install zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
     echo "Installing Verilator version v5.028..."
     git clone https://github.com/verilator/verilator.git $HOME/verilator
     cd $HOME/verilator
